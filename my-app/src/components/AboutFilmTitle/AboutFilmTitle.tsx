4ce037e93eb0146b35b580806ea5c662dd5e6137
Nikita <chirkov31011993@gmail.com>
2024-10-01 22:24:31 +0500

    12.1 commit

--- my-app/src/components/AboutFilmTitle/AboutFilmTitle.tsx
+++ my-app/src/components/AboutFilmTitle/AboutFilmTitle.tsx
@@ -1,4 +1,3 @@
-import { skipToken } from "@reduxjs/toolkit/query/react";
 import { useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import { useMovieIdQuery, useMovieRandomQuery } from "../../api/cinemaGuideApi";
@@ -15,12 +14,27 @@ const AboutFilmTitle = ({ flag }: { flag: boolean }) => {
     const { id } = useParams<{ id: string }>();
     const movieId = Number(id);
 
-    const { data: randomMovieData, refetch:refetchrandomMovie  } = useMovieRandomQuery();
-    const { data, isFetching, error, } = useMovieIdQuery(randomMovieData?.id && (movieId) ? movieId : skipToken);
+    const { data: randomMovieData, refetch: refetchrandomMovie, isLoading: isLoadingRandom,error:errorRandom } = useMovieRandomQuery();
+    console.log(randomMovieData,"randomMovieData");
+
+    const { data, isFetching, error } = useMovieIdQuery(movieId );
 
     // console.log(data, "data");
+    if (isLoadingRandom) {
+        return <Loading />; // Можно отобразить индикатор загрузки
+    }
+
+    if (!randomMovieData) {
+        return <div>No data available2323232</div>; // Если данные все еще недоступны
+    }
+
+    if (errorRandom) {
+        return <div>Error: {error.message}</div>;
+        // Обработка ошибок
+    }
+
     if (isFetching) {
-        return <Loading/>; // Можно отобразить индикатор загрузки
+        return <Loading />; // Можно отобразить индикатор загрузки
     }
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -31,7 +45,7 @@ const AboutFilmTitle = ({ flag }: { flag: boolean }) => {
         return <div>No data available</div>; // Если данные все еще недоступны
     }
     // Логика определения цвета рейтинга
-    const getRatingColor = ( rating: number ) => {
+    const getRatingColor = (rating: number) => {
         if (rating >= 0 && rating <= 4.2) return "#c82020";
         if (rating > 4.3 && rating <= 6.3) return "#777";
         if (rating > 6.3 && rating <= 7.5) return "#308e21";
@@ -48,59 +62,78 @@ const AboutFilmTitle = ({ flag }: { flag: boolean }) => {
     return (
         <div className={style.content}>
             <div className={style.info__wrapper}>
-                <div className={style.info__rating}>
-
-                    <div className={style.rating} style={{ backgroundColor: ratingColor }}>
-                        <img
-                            className={style.rating__star}
-                            src="/imgs/starRaiting.svg" alt="рейтинг"
-                        />
-                        <div className={style.rating__number}>
-                            {data.tmdbRating}
-                        </div>
-                    </div>
-
-                    <div className={style.info}>
-                        <span>{data.releaseYear} year&nbsp; </span>
-                        <div>
-                            {data.genres.map((item, index) => (
-                                <span key={index}>({item})</span>
-                            ))}
-                        </div>
-                        <div>&nbsp;{data.runtime} min</div>
-                    </div>
-                </div>
-                <h2 className={style.title}>{data.title}</h2>
-                <p className={style.description}>{data.plot}</p>
-                <ModalTrailer movie={data.trailerYouTubeId} modalTrailer={modalTrailer} setModalTrailer={setModalTrailer} />
                 {flag ? (
-                    <div className={style.btn__wrapper}>
-                        <div className={style.btn__trailer}>
-                            <BtnBrandActive onClick={() => setModalTrailer(true)}> Трейлер</BtnBrandActive>
-                        </div>
+                    <>
+                        <h2 className={style.title}>{randomMovieData?.title}</h2>
+                        <p className={style.description}>{randomMovieData?.plot}</p>
+                        <ModalTrailer movie={randomMovieData?.trailerYouTubeId} modalTrailer={modalTrailer} setModalTrailer={setModalTrailer} />
+                        <div className={style.info__rating}>
+                            <div className={style.rating} style={{ backgroundColor: ratingColor }}>
+                                <img className={style.rating__star} src="/imgs/starRaiting.svg" alt="рейтинг" />
+                                <div className={style.rating__number}>{randomMovieData?.tmdbRating}</div>
+                            </div>
 
-                        <div className={style.btn__not_trailer}>
-                            <NavLink to={`/aboutFilm/${randomMovieData?.id}`}>
-                                <BtnAboutFilm />
-                            </NavLink>
-
-                            <BtnFavorites />
-                            <BtnMix onClick={handleMovieMix} />
+                            <div className={style.info}>
+                                <span>{randomMovieData?.releaseYear} year&nbsp; </span>
+                                <div>
+                                    {randomMovieData?.genres.map((item, index) => (
+                                        <span key={index}>({item})</span>
+                                    ))}
+                                </div>
+                                <div>&nbsp;{randomMovieData?.runtime} min</div>
+                            </div>
                         </div>
-                    </div>
+                        <div className={style.btn__wrapper}>
+                            <div className={style.btn__trailer}>
+                                <BtnBrandActive onClick={() => setModalTrailer(true)}> Трейлер</BtnBrandActive>
+                            </div>
+
+                            <div className={style.btn__not_trailer}>
+                                <NavLink to={`/aboutFilm/${randomMovieData?.id}`}>
+                                    <BtnAboutFilm />
+                                </NavLink>
+
+                                <BtnFavorites />
+                                <BtnMix onClick={handleMovieMix} />
+                            </div>
+                        </div>
+                    </>
                 ) : (
-                    <div className={style.btn__wrapper_2}>
-                        <div className={style.btn__trailer_2}>
-                            <BtnBrandActive onClick={() => setModalTrailer(true)}>Трейлер</BtnBrandActive>
+                        <>
+                            <h2 className={style.title}>{data.title}</h2>
+                        <p className={style.description}>{data.plot}</p>
+                        <ModalTrailer movie={data.trailerYouTubeId} modalTrailer={modalTrailer} setModalTrailer={setModalTrailer} />
+                        <div className={style.info__rating}>
+                            <div className={style.rating} style={{ backgroundColor: ratingColor }}>
+                                <img className={style.rating__star} src="/imgs/starRaiting.svg" alt="рейтинг" />
+                                <div className={style.rating__number}>{data.tmdbRating}</div>
+                            </div>
+
+                            <div className={style.info}>
+                                <span>{data.releaseYear} year&nbsp; </span>
+                                <div>
+                                    {data.genres.map((item, index) => (
+                                        <span key={index}>({item})</span>
+                                    ))}
+                                </div>
+                                <div>&nbsp;{data.runtime} min</div>
+                            </div>
                         </div>
-                        <div className={style.btn__not_trailer}>
-                            <BtnFavorites />
-                        </div>
-                    </div>
+                        <div className={style.btn__wrapper_2}>
+                            <div className={style.btn__trailer_2}>
+                                <BtnBrandActive onClick={() => setModalTrailer(true)}>Трейлер</BtnBrandActive>
+                            </div>
+                            <div className={style.btn__not_trailer}>
+                                <BtnFavorites />
+                            </div>
+                            </div>
+
+                    </>
                 )}
             </div>
-
             <div className={style.info__background} style={{ backgroundImage: `url(${data.backdropUrl})` }}></div>
+
+
         </div>
     );
 };
