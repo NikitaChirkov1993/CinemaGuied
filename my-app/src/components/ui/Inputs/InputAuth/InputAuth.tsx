e562a6c3216a5df727a99ab75e97dd0c2308904a
Nikita <chirkov31011993@gmail.com>
2024-10-05 01:01:46 +0500

    15.3 commit

--- my-app/src/components/ui/Inputs/InputAuth/InputAuth.tsx
+++ my-app/src/components/ui/Inputs/InputAuth/InputAuth.tsx
@@ -1,60 +1,58 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { InputAuthProps } from "../../../../types/types";
 import style from "./inputAuth.module.css";
 
+const InputAuth: React.FC<InputAuthProps> = ({ img, type, placeholder, auth, setAuth, isName, setErrorMassage, errorMassage }) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setAuth((prev) => ({ ...prev, [isName]: e.target.value }));
+        setErrorMassage((prev) => ({
+            ...prev,
+            flagGlobal: false,
+            flagEmail: false,
+            flagPass: false,
+            errorMassage: "",
+        }));
+        // setFlagError(false);
+        // setFlagPass(false);
+    };
 
+    const [isFocused, setIsFocused] = useState(false); // Состояние для фокуса
+    const handleFocus = () => {
+        setIsFocused(true); // Устанавливаем состояние фокуса
+    };
 
-const InputAuth: React.FC<InputAuthProps> = ({
-  img,
-  type,
-  placeholder,
-  auth,
-  setAuth,
-  isName,
-  setErrorMassage,
-  errorMassage,
-}) => {
+    const handleBlur = () => {
+        setIsFocused(false); // Сбрасываем состояние при потере фокуса
+    };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setAuth((prev) => ({ ...prev, [isName]: e.target.value }));
-      setErrorMassage((prev) => (
-          {
-              ...prev,
-              flagGlobal: false,
-              flagEmail: false,
-              flagPass: false,
-              errorMassage: ""
-          }));
-    // setFlagError(false);
-    // setFlagPass(false);
-  };
+    const rootClasses = [style.auth];
+    if (errorMassage?.flagGlobal && !auth) {
+        rootClasses.push(style.active);
+    }
+    if (isFocused) {
+        rootClasses.push(style.activeFocus);
+    }
+    if (errorMassage?.flagPass && (isName === "password" || isName === "passwordRepeat")) {
+        rootClasses.push(style.activePass);
+    }
+    if (errorMassage?.flagEmail && isName === "email") {
+        rootClasses.push(style.activePass);
+    }
 
-  const rootClasses = [style.auth];
-  if (errorMassage?.flagGlobal && !auth) {
-    rootClasses.push(style.active);
-  }
-  if (auth && !((errorMassage.flagPass) && (errorMassage.flagEmail)  )) {
-    rootClasses.push(style.activeFocus);
-  }
-  if (errorMassage?.flagPass && (isName === "password" || isName === "passwordRepeat")) {
-    rootClasses.push(style.activePass);
-  }
-  if (errorMassage?.flagEmail && (isName === "email" )) {
-    rootClasses.push(style.activePass);
-  }
-
-  return (
-    <div className={rootClasses.join(" ")}>
-      <img className={style.img__auth} src={img} alt="icon" />
-      <input
-        value={auth}
-        onChange={handleChange}
-        placeholder={placeholder}
-        className={style.input__auth}
-        type={type}
-      />
-    </div>
-  );
+    return (
+        <div className={rootClasses.join(" ")}>
+            <img className={style.img__auth} src={img} alt="icon" />
+            <input
+                value={auth}
+                onChange={handleChange}
+                placeholder={placeholder}
+                className={style.input__auth}
+                type={type}
+                onFocus={handleFocus} // Добавляем обработчик фокуса
+                onBlur={handleBlur} // Добавляем обработчик потери фокуса
+            />
+        </div>
+    );
 };
 
 export default InputAuth;
