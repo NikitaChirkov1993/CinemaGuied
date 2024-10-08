f9bb333ac564b23e40e7e48f7e05917f1c5d6467
Nikita <chirkov31011993@gmail.com>
2024-10-08 12:42:10 +0500

    18.1 commit

--- my-app/src/components/ui/Buttons/BtnFavorites/BtnFavorites.tsx
+++ my-app/src/components/ui/Buttons/BtnFavorites/BtnFavorites.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { useGetFavoritesQuery, usePostFavoritesMutation } from "../../../../api/cinemaGuideApi";
+import { useDeleteFavoritesMutation, useGetFavoritesQuery, usePostFavoritesMutation } from "../../../../api/cinemaGuideApi";
+import Loading from "../../Loading/Loading";
 import style from "./BtnFavorites.module.css";
 
 const BtnFavorites = ({ id }: { id: number  }) => {
@@ -8,12 +9,10 @@ const BtnFavorites = ({ id }: { id: number  }) => {
     const { data:dataGetFavorites,refetch:refetchGetFavorites } = useGetFavoritesQuery();
     const isFavoritesInitial = dataGetFavorites?.find((item) => item.id === id) ? true : false;
     const [isFavorites, setIsFavorites] = useState(isFavoritesInitial);
-    // console.log(id, "ID");
-    // console.log(isFavoritesInitial, "isFavoritesInitial");
 
     //ДОБОВЛЕНИЕ ФИЛЬМА:
-    const [postFavorites, { isSuccess }] = usePostFavoritesMutation();
-    const handleToggleFavorites = async () => {
+    const [postFavorites, { isLoading:isLoadingPostFavorites }] = usePostFavoritesMutation();
+    const handleAddFavorites = async () => {
 
         try {
             await postFavorites(favoritesID).unwrap();
@@ -24,35 +23,42 @@ const BtnFavorites = ({ id }: { id: number  }) => {
         }
     };
 
-
     //УДАЛЕНИЕ ФИЛЬМА:
-    // const [deleteFilm, { isLoading: isLoadingDelete }] = useDeleteFavoritesMutation();
-
-    // if (isLoadingDelete) {
-    //     return <Loading/>
-    // }
-    // const handeleDelete = async () => {
-    //     try {
-    //         await deleteFilm(movieId).unwrap();
-    //         refetchFavorites();
-    //     } catch (error) {
-    //         console.error("Ошибка при удаление", error);
-    //     }
-
-    //     // window.location.reload();
-    // };
+    const [deleteFilm, { isLoading: isLoadingDelete }] = useDeleteFavoritesMutation();
+    const handeleDeleteFavorites = async () => {
+        try {
+            await deleteFilm(id).unwrap();
+            setIsFavorites((prev) => !prev);
+            refetchGetFavorites();
+        } catch (error) {
+            console.error("Ошибка при удаление", error);
+        }
+    };
 
 
     return (
+        <>
+        {!isFavorites ?
+            <button className={style.favorites} onClick={handleAddFavorites}>
+                {isLoadingPostFavorites ? <Loading /> : <img src="/imgs/on=false.svg" alt="Не в избранном" />}
+            </button>
+            :
+            <button className={style.favorites} onClick={handeleDeleteFavorites }>
+                {isLoadingDelete ? <Loading /> : <img src="/imgs/on=true.svg" alt="В избранном" />}
+            </button>
+        }
+    </>
 
-        <button className={style.favorites} onClick={handleToggleFavorites}>
+
+    );
+};
+
+export default BtnFavorites;
+
+{/* <button className={style.favorites} onClick={handleToggleFavorites}>
             {!isFavorites ?
                 <img src="/imgs/on=false.svg" alt="Не в избранном" />
              :
                 <img src="/imgs/on=true.svg" alt="В избранном" />
             }
-        </button>
-    );
-};
-
-export default BtnFavorites;
+        </button> */}
