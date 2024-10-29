fb7ba6bcc8c89f13c1103bfb6972a98f0106846d
Nikita <chirkov31011993@gmail.com>
2024-10-29 21:12:09 +0500

    33.2 commit

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,7 +4,4 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    devSourcemap: false, // Отключает создание source maps для CSS в режиме разработки
-  },
 })
